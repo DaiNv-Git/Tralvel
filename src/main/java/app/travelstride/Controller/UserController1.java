f80614bbe5fa9e68ee6ecdf928ebd9ecf32ebcdd
daidev <dainv.220301@gmail.com>
2025-03-20 17:04:03 +0700

    add

---
+++ src/main/java/app/travelstride/Controller/UserController1.java
@@ -0,0 +1,9 @@
+package app.travelstride.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users1")
+public class UserController1 {
+}
