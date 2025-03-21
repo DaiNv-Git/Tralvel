e8d66634ca7d547880b0dae2f16e669f9a441a64
daidev <dainv.220301@gmail.com>
2025-03-21 13:19:32 +0700

    fix config

--- src/main/java/app/travelstride/Config/WebConfig.java
+++ src/main/java/app/travelstride/Config/WebConfig.java
@@ -7,11 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:uploads/images/");
-    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Áp dụng cho tất cả endpoint
